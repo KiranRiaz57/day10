f8361627c903af3c97c995b605b08db393c1f30c
KiranRiaz57 <159231693+KiranRiaz57@users.noreply.github.com>
2024-03-13 22:23:01 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,41 @@
+// // Q28
+// let age = 25;
+// if (age <2){
+//     console.log("The person is a baby.")
+// }
+// else if(age <4){
+//     console.log("The person is a toddler.")
+// }
+// else if(age <13){
+//     console.log("The person is a kid.")
+
+// }
+// else if(age <20){
+//     console.log("The person is a teenager.")
+// }
+// else if (age <65){
+//     console.log("The person is a adult.")
+// }
+// else {
+//     console.log("The person is an elder.")
+// }
+// // Q29
+// let favourite_fruits: string[] = ["mango","cherries","pomegranate"]
+// if(favourite_fruits.includes("cherries")){
+//     console.log("You really like cherries!");
+// }
+// if(favourite_fruits.includes("mango")){
+//     console.log("You really like mango!");
+// }
+// if(favourite_fruits.includes("pomegranate")){
+//     console.log("You really like pomegranate!");
+// }
+// Q30
+// let userNames: string[] = ["admin","user1","user2","user3","user4"]
+// userNames.forEach(userName=>{
+    // if(userName==="admin"){
+        // console.log("hello admin,would you like to see a status report?")
+    // }else{
+        // console.log("hello",userName,"thank you for logging in again.")
+    // }
+// })
